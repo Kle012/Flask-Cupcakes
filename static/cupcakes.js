dddf359ce07246a83ecab1e7d8ea3a8712a5191b
Kle012 <kimhle.0102@gmail.com>
2023-01-30 17:24:35 -0800

    Static files

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -5,12 +5,12 @@ function generateCupcakeHTML(cupcake) {
     return `
     <div data-cupcake-id = ${cupcake.id}>
         <li>
-        Flavor: ${cupcake.flavor},
-        Size: ${cupcake.size},
+        Flavor: ${cupcake.flavor} -- 
+        Size: ${cupcake.size} --
         Rating: ${cupcake.rating}
         <button class='delete-button'>X</button>
         </li>
-        <img class='Cupcake-img'
+        <img class='cupcake-img'
             src='${cupcake.image}'
             alt='(no image provided)'> 
     </div>
@@ -27,6 +27,32 @@ async function showListCupcakes() {
 }
 
 $('#new-cupcake-form').on('submit', async function (e) {
+    // Handle form for adding a new cupcake.
     e.preventDefault();
-    
-})
+    let flavor = $('#flavor').val();
+    let size = $('#size').val();
+    let rating = $('#rating').val();
+    let image = $('#image').val();
+
+    const newData = await axios.post(`${BASE_URL}/cupcakes`, {
+        flavor,
+        size,
+        rating,
+        image
+    });
+
+    let newCupcake = $(generateCupcakeHTML(newData.data.cupcake));
+    $('#cupcakes-list').append(newCupcake);
+    $('#new-cupcake-form').trigger('reset'); 
+});
+
+$('#cupcakes-list').on('click', '.delete-button', async function (e) {
+    e.preventDefault();
+    let $cupcake = $(e.target).closest('div');
+    let cupcakeId = $cupcake.attr('data-cupcake-id');
+
+    await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
+    $cupcake.remove();
+});
+
+$(showListCupcakes); 
